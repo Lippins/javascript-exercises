c36aa0386907b11f46fb424f2be5a560f5cf9886
Israel Ogunmola <israelogunmola@gmail.com>
2023-08-15 20:02:44 +0100

    Complete exercises 11 and 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,16 @@
-const findTheOldest = function() {
+const findTheOldest = function (entries) {
+  result = entries.sort((a, b) => {
+    let ageA = !a.yearOfDeath
+      ? new Date().getFullYear() - a.yearOfBirth
+      : a.yearOfDeath - a.yearOfBirth;
+    let ageB = !b.yearOfDeath
+      ? new Date().getFullYear() - b.yearOfBirth
+      : b.yearOfDeath - b.yearOfBirth;
 
+    return ageB - ageA;
+  });
+
+  return result[0];
 };
 
 // Do not edit below this line
